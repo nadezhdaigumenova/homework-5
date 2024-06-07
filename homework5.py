09dd3230e1010d249da38a9a032eadbddb877ab5
nadezhdaigumenova <nadya.kyzlakova@mail.ru>
2024-06-07 23:00:07 +0700

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var = tuple = 'apple', 3, True
+print(immutable_var)
+#tuple[1] = [30] # кортеж нельзя изменить, нам нужно чтобы он оставался неизменный
+mutable_list = ['car', 'var', 'peace']
+mutable_list[0] = 'quiet'
+print(mutable_list)
